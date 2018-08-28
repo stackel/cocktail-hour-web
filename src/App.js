2f2faafb0452f3e6c9dfc1c4f41f2bbfda93d0e1
Erik Stackelberg <stackelberg@splitstockholm.se>
2018-08-28 19:39:25 +0200

    Log in and out.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+
+import {database, auth, googleAuthProvider} from './firebase'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null,
+      firebaseUser: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log(
+          "Successfully logged in with authUser " + authUser.displayName + " " + auth.uid
+        )
+        console.log(authUser)
+        this.setState({authUser: authUser})
+
+        //find firestore user by uid.
+        let userRef = database.collection("users").doc(authUser.uid);
+
+        userRef.onSnapshot(snapshot => {
+          let firestoreUser = snapshot.data()
+          if (firestoreUser) {
+            console.log("Found firestore user.")
+            console.log(firestoreUser)
+            this.setState({firestoreUser: firestoreUser})
+          } else {
+            console.log("Did not find firestore user")
+          }
+        })
+      } else {
+        console.log("Auth user not found.")
+      }
+    });
+  }
+
+  loginWithGoogleAuth = () => {
+    auth.signInWithPopup(googleAuthProvider).then(result => {
+      console.log("Google auth successful with result: ")
+      console.log(result)
+    }).catch(error => {
+      console.log("Google auth failed with error: ")
+      console.log(error)
+    })
+  }
+
+  logout = () => {
+    auth.signOut().then(() =>  {
+      console.log("Successfully logged out.")
+      this.setState({
+        authUser: null,
+        firebaseUser: null
+      })
+    }, error => {
+      console.log("An error occured while logging out: ")
+      console.log(error)
+    })
+  }
+
+  UserArea = (props) => {
+    if (!props.authUser) {
+      return (
+        <Button onClick={this.loginWithGoogleAuth} variant="contained" color="primary">
+          Log in
+        </Button>
+      )
+    } else {
+      return (
+        <Button onClick={this.logout} variant="contained" color="primary">
+          Log out
+        </Button>
+      )
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div>
+      <this.UserArea authUser={this.state.authUser}/>
+    </div>);
   }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+const firebase = require("firebase");
+
+require("firebase/app");
+require("firebase/firestore");
+require("firebase/auth");
+
+let config = {
+  apiKey: "AIzaSyBFm-wn2F0YqF-7xguCk6Lj7gSMr6JiXvA",
+  authDomain: "cocktail-hour-1529929123157.firebaseapp.com",
+  databaseURL: "https://cocktail-hour-1529929123157.firebaseio.com",
+  projectId: "cocktail-hour-1529929123157",
+  storageBucket: "cocktail-hour-1529929123157.appspot.com",
+  messagingSenderId: "295959505102"
+};
+
+let settings = { timestampsInSnapshots: true}
+
+firebase.initializeApp(config);
+
+let database = firebase.firestore();
+database.settings(settings)
+
+let googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+let auth = firebase.auth();
+
+export { database, auth, googleAuthProvider };
