30b01c15e8f60cb9def6f2d287ce7e611fa491ed
Erik Stackelberg <stackelberg@splitstockholm.se>
2018-09-19 14:54:56 +0200

    profile page

--- src/components/dashboard/User.jsx
+++ src/components/dashboard/User.jsx
@@ -4,71 +4,46 @@ import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Loading from 'components/shared/Loading'
 
+import { auth } from 'utils/firebase'
 class User extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {
-      drawerOpen: false
+    this.state = {
+      user: null
+    }
+
+  }
+
+  componentDidMount() {
+    if(this.props.location.state) {
+      this.setState({
+        user: JSON.parse(this.props.location.state.authUser)
+      })
+    } else {
+      this.fetchUser()
     }
   }
 
-  onLogOutClicked = () => {
-    this.props.onLogout()
+  fetchUser = () => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.setState({user: authUser})
+      }
+    })
   }
 
-  toggleDrawer = (open) => () => {
-    this.setState({
-      drawerOpen: open,
-    });
-  };
-
   render() {
-    if (!this.props.authUser) {
-      return null
+    const user = this.state.user
+    if (!user) {
+      return (<div class="tc mt6"><Loading/> </div>)
     } else {
       return (
-        <div>
-          <Drawer
-            anchor="top"
-            open={this.state.drawerOpen}
-            onClose={this.toggleDrawer(false)}>
-            <div
-              tabIndex={0}
-              role="button"
-              onClick={this.toggleDrawer(false)}
-              onKeyDown={this.toggleDrawer(false)}>
-
-              <List component="nav">
-
-                <ListItem>
-                  <p className="sans-serif f4 mv0 pv0">
-                    {this.props.authUser.displayName}
-                  </p>
-                </ListItem>
-
-                <ListItem>
-                  <p className="sans-serif f4 gray mv0 pv0 fw3">
-                    {this.props.authUser.email}
-                  </p>
-                </ListItem>
-
-                <ListItem button="button">
-                  <Button onClick={this.props.onLogout} variant="contained" color="primary">
-                    Log out
-                  </Button>
-                </ListItem>
-              </List>
-
-            </div>
-          </Drawer>
-          <div className="tc">
-            <Button
-              className="sans-serif"
-              variant="outlined"
-              onClick={this.toggleDrawer(true)}>
-              {this.props.authUser.displayName}</Button>
-          </div>
+        <div className="pa4">
+          <h1 className="sans-serif f2 mt0 mb4">Profile</h1>
+          <h2 className="sans-serif f3 mb0">{user.displayName}</h2>
+          <h2 className="sans-serif f4 fw3 gray">{user.email}</h2>
         </div>
       )
     }
