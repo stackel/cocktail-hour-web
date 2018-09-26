d8c4394b3065c7832b744b33250fc223350c7051
Erik Stackelberg <stackelberg@splitstockholm.se>
2018-09-26 21:40:46 +0200

    email/pw

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
 
 import {database, auth, googleAuthProvider} from 'utils/firebase'
 import Loading from 'components/shared/Loading'
@@ -47,6 +49,20 @@ class Auth extends Component {
     });
   }
 
+  emailChanged = event => {
+    this.setState({
+      email: event.target.value,
+      errorMessage: ""
+
+    })
+  }
+
+  passwordChanged = event => {
+    this.setState({
+      password: event.target.value,
+      errorMessage: ""
+    })
+  }
 
   loginWithGoogleAuth = () => {
     this.setState({loading: true})
@@ -54,8 +70,46 @@ class Auth extends Component {
       console.log("Google auth successful with result: ")
       console.log(result)
     }).catch(error => {
+      this.setState({loading: false})
       console.log("Google auth failed with error: ")
       console.log(error)
+      this.setState({
+        errorMessage: error.message
+      })
+    })
+  }
+
+  loginWithEmailPassword = () => {
+    this.setState({loading: true})
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password).then(
+      result => {
+        console.log("Email/Password auth successful with result: ")
+        console.log(result)
+      }
+    ).catch(error => {
+      this.setState({loading: false})
+      console.log("Email/Password auth failed with error: ")
+      console.log(error)
+      this.setState({
+        errorMessage: error.message
+      })
+    })
+  }
+
+  registerWithEmailAndPassword = () => {
+    this.setState({loading: true})
+    auth.createUserWithEmailAndPassword(this.state.email, this.state.password).then(
+      result => {
+        console.log("Email/Password register successful with result: ")
+        console.log(result)
+      }
+    ).catch(error => {
+      this.setState({loading: false})
+      console.log("Email/Password register failed with error: ")
+      console.log(error)
+      this.setState({
+        errorMessage: error.message
+      })
     })
   }
 
@@ -80,9 +134,56 @@ class Auth extends Component {
       <div className="tc">
         <h1 className="sans-serif f2 tc mt7">
           COCKTAIL HOUR</h1>
-        <Button onClick={this.loginWithGoogleAuth} variant="outlined" color="primary">
-          Log in
-        </Button>
+        <div>
+          <div>
+            <TextField
+              defaultValue={this.state.email}
+              id="email"
+              label="Email"
+              type="email"
+              autoComplete="email"
+              onChange={this.emailChanged}/>
+          </div>
+          <div className="mt2 mb4">
+            <TextField
+              defaultValue={this.state.password}
+              id="password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              onChange={this.passwordChanged}/>
+          </div>
+
+          <div>
+            <div className="dib mh2">
+
+              <Button
+                onClick={this.loginWithEmailPassword}
+                variant="contained"
+                color="primary">
+                Sign in
+              </Button>
+            </div>
+            <div className="dib mh2">
+
+              <Button
+                onClick={this.registerWithEmailAndPassword}
+                className="mh2"
+                variant="outlined"
+                color="primary">
+                Register
+              </Button>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="mv4">
+          <Button onClick={this.loginWithGoogleAuth} variant="contained" color="primary">
+            Sign in / register with Google
+          </Button>
+        </div>
+        <p className="tc dark-red sans-serif f5"> {this.state.errorMessage}</p>
       </div>
     )
   }
